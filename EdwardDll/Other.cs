595c78352885e8906befda9a9b80bac37f1e3331
Edward <edward_song@yeah.net>
2017-04-17 19:37:24 +0800

    add delete myself,v1.0.1.8

--- EdwardDll/Other.cs
+++ EdwardDll/Other.cs
@@ -4,6 +4,8 @@ using System.Linq;
 using System.Text;
 using System.IO;
 using System.Net.NetworkInformation;
+using System.Diagnostics;
+using System.Windows.Forms;
 
 namespace Edward
 {
@@ -120,6 +122,29 @@ namespace Edward
                 throw new Exception("Character is not valid.");
             }
         }
+        #endregion
+
+        #region DeleteMyself
+
+
+        /// <summary>
+        /// delete myselef
+        /// </summary>
+        public static void DelMe()
+        {
+            string fileName = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "remove.bat");
+
+            StreamWriter bat = new StreamWriter(fileName, false, Encoding.Default);
+            bat.WriteLine(string.Format("del \"{0}\" /q", Application.ExecutablePath));
+            bat.WriteLine(string.Format("del \"{0}\" /q", fileName));
+            bat.Close();
+            File.SetAttributes(fileName, FileAttributes.Hidden);
+            ProcessStartInfo info = new ProcessStartInfo(fileName);
+            info.WindowStyle = ProcessWindowStyle.Hidden;
+            Process.Start(info);
+            Environment.Exit(0);
+        }
+
         #endregion
 
     }
